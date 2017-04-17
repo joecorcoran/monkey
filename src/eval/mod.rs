4b6c61ef1b8debfa2b364dcb5c96374eb50f5979
Joe Corcoran <joe@corcoran.io>
2017-04-17 20:24:20 +0200

    Conditionals

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -22,15 +22,32 @@ pub trait Eval {
 
 impl Eval for Program {
     fn eval(&self) -> EvalResult {
-	let results = self.statements.iter().map(|s| s.eval());
-	results.collect::<Vec<_>>().pop().unwrap_or(Err(Error::NoInput))
+	let mut result = Ok(NULL);
+	for s in &self.statements {
+	    result = s.eval();
+	    if result.is_err() { break }
+	}
+	result
     }
 }
 
 impl Eval for Statement {
     fn eval(&self) -> EvalResult {
 	match *self {
-	    Statement::Expression { expression: ref e } => { e.eval() },
+	    Statement::Expression { expression: ref e } => e.eval(),
+	    Statement::Block { statements: ref ss } => {
+		match *ss {
+		    Some(ref ss) => {
+			let mut result = Ok(NULL);
+			for s in ss {
+			    result = s.eval();
+			    if result.is_err() { break }
+			}
+			result
+		    },
+		    None => Ok(NULL)
+		}
+	    },
 	    _ => Err(Error::NotImplemented)
 	}
     }
@@ -43,6 +60,7 @@ impl Eval for Expression {
 	    Expression::Boolean(b) => Ok(if b { TRUE } else { FALSE }),
 	    Expression::Prefix { operator: ref o, right: ref r } => eval_prefix(o, r),
 	    Expression::Infix { left: ref l, operator: ref o, right: ref r } => eval_infix(l, o, r),
+	    Expression::If { condition: ref c, consequence: ref cq, alternative: ref a } => eval_if(c, cq, a),
 	    _ => Err(Error::NotImplemented)
 	}
     }
@@ -109,10 +127,30 @@ fn eval_boolean_infix(left: bool, operator: &Token, right: bool) -> EvalResult {
     }
 }
 
+fn eval_if(condition: &Box<Expression>, consequence: &Box<Statement>, alternative: &Option<Box<Statement>>) -> EvalResult {
+    let condition_value = condition.eval();
+    if condition_value.is_err() { return condition_value }
+    if truthy(condition_value.unwrap()) {
+	consequence.eval()
+    } else {
+	match *alternative {
+	    Some(ref a) => a.eval(),
+	    None => Ok(NULL)
+	}
+    }
+}
+
 fn boolean(b: bool) -> Object {
     match b {
 	true => TRUE,
 	false => FALSE
+    }
+}
+
+fn truthy(object: Object) -> bool {
+    match object {
+	FALSE | NULL => false,
+	_ => true
     }
 }
 
@@ -218,6 +256,52 @@ mod test {
 	    ]
 	};
 	let expected = Ok(Object::Boolean(false));
+	assert_eq!(expected, program.eval());
+    }
+
+    #[test]
+    fn conditional_if() {
+	let program = Program {
+	    statements: vec![
+		Statement::Expression {
+		    expression: Expression::If {
+			condition: Box::new(Expression::Boolean(true)),
+			consequence: Box::new(Statement::Block {
+			    statements: Some(vec![
+				Box::new(Statement::Expression { expression: Expression::Integer(10) })
+			    ])
+			}),
+			alternative: None
+		    }
+		}
+	    ]
+	};
+	let expected = Ok(Object::Integer(10));
+	assert_eq!(expected, program.eval());
+    }
+
+    #[test]
+    fn conditional_if_else() {
+	let program = Program {
+	    statements: vec![
+		Statement::Expression {
+		    expression: Expression::If {
+			condition: Box::new(Expression::Boolean(false)),
+			consequence: Box::new(Statement::Block {
+			    statements: Some(vec![
+				Box::new(Statement::Expression { expression: Expression::Integer(10) })
+			    ])
+			}),
+			alternative: Some(Box::new(Statement::Block {
+			    statements: Some(vec![
+				Box::new(Statement::Expression { expression: Expression::Integer(20) })
+			    ])
+			}))
+		    }
+		}
+	    ]
+	};
+	let expected = Ok(Object::Integer(20));
 	assert_eq!(expected, program.eval());
     }
 }
