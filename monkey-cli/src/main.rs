5e65c8109fbd83bf6c95f140d7553a3067f9b8ff
Joe Corcoran <joe@corcoran.online>
2019-01-07 21:51:25 +0100

    Read code from file in cli

--- monkey-cli/src/main.rs
+++ monkey-cli/src/main.rs
@@ -2,13 +2,42 @@ extern crate rustyline;
 extern crate monkey_core;
 
 use monkey_core::{Env, Eval, Lexer, Parser};
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {
-    // TODO switch on argv between running file/stdin and repl
-    repl()
+fn main() -> std::io::Result<()> {
+    let argv: Vec<String> = env::args().collect();
+    if let Some(path) = argv.get(1) {
+	eval_file(path)
+    } else {
+	repl()
+    }
 }
 
-fn repl() {
+fn eval_file(path: &str) -> std::io::Result<()> {
+    let mut file = File::open(path)?;
+    let mut code = String::new();
+    file.read_to_string(&mut code)?;
+
+    let env = Env::env_ref(None);
+    let mut lexer = Lexer::new(&code);
+    let mut parser = Parser::new(&mut lexer);
+
+    match parser.parse() {
+	Ok(ast) => {
+	    let result = ast.eval(env.clone());
+	    match result {
+		Ok(object) => println!("=> {}", object),
+		Err(error) => println!("Error: {:?}", error)
+	    }
+	},
+	Err(error) => println!("Error: {}", error)
+    }
+    Ok(())
+}
+
+fn repl() -> std::io::Result<()> {
     let mut rl = rustyline::Editor::<()>::new();
     let env = Env::env_ref(None);
     loop {
@@ -22,7 +51,7 @@ fn repl() {
 		    Ok(ast) => {
 			let result = ast.eval(env.clone());
 			match result {
-			    Ok(object) => println!("{}", object),
+			    Ok(object) => println!("=> {}", object),
 			    Err(error) => println!("Error: {:?}", error)
 			}
 		    },
@@ -32,4 +61,5 @@ fn repl() {
 	    Err(_) => println!("No input"),
 	}
     }
+    Ok(())
 }
